e6c8b285fe29a4e609de2828945d655010b2e7fb
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2025-09-20 20:28:07 -0300

    Feito o create do CRUD

--- resources/views/produtos/create.blade.php
+++ resources/views/produtos/create.blade.php
@@ -1,8 +1,13 @@
 <h1>Novo Produto</h1>
 <form action="{{ route('produtos.store') }}" method="POST">
     @csrf
-    Nome: <input type="text" name="nome"><br>
-    Quantidade: <input type="number" name="quantidade"><br>
-    Preço: <input type="text" name="preco"><br>
-    <button type="submit">Salvar</button>
+    <label for="nome">Nome:</label>
+    <input type="text" id="nome" name="nome"><br>
+
+    <label for="quantiddade">Quantidade:</label>
+    <input type="number" id="quantidade" name="quantidade"><br>
+    
+    <label for="preco">Preço:</label>
+    <input type="text" id="preco" name="preco"><br>
+    <button type="submit">Cadastrar</button>
 </form>
