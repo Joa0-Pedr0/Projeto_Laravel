de5de4bac850c22e6f05803905348c681973682b
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2025-09-20 20:18:06 -0300

    Feito o edit do CRUD

--- resources/views/produtos/edit.blade.php
+++ resources/views/produtos/edit.blade.php
@@ -1,8 +1,11 @@
 <h1>Editar Produto</h1>
 <form action="{{ route('produtos.update', $produto) }}" method="POST">
     @csrf @method('PUT')
-    Nome: <input type="text" name="nome" value="{{ $produto->nome }}"><br>
-    Quantidade: <input type="number" name="quantidade" value="{{ $produto->quantidade }}"><br>
-    Preço: <input type="text" name="preco" value="{{ $produto->preco }}"><br>
+    <label for="nome">Nome:</label>
+    <input type="text" id="nome" name="nome" value="{{ $produto->nome }}"><br>
+    <label for="quantidade">Quantidade:</label>
+    <input type="number" name="quantidade" id="quantidade" value="{{ $produto->quantidade }}"><br>
+    <label for="preco">Preço:</label>
+    <input type="text" name="preco"  id= "preco" value="{{ $produto->preco }}"><br>
     <button type="submit">Salvar</button>
 </form>
