dcd224eca69742307cc9a5f1328af993f58ffb0f
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2025-10-04 10:16:58 -0300

    adicionado no index a mensagem de sucesso

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -1,23 +1,30 @@
 <h1>Estoque</h1>
 <a href="{{ route('produtos.create') }}">Novo Produto</a>
 
+@if(session('success'))
+    <p style="color: green;">{{ session('success') }}</p>
+@endif
+
 <table border="1" cellpadding="5">
     <tr>
         <th>ID</th><th>Nome</th><th>Qtd</th><th>Preço</th><th>Ações</th>
     </tr>
+
     @foreach($produtos as $p)
-    <tr>
-        <td>{{ $p->id }}</td>
-        <td>{{ $p->nome }}</td>
-        <td>{{ $p->quantidade }}</td>
-        <td>R$ {{ $p->preco }}</td>
-        <td>
-            <a href="{{ route('produtos.edit', $p) }}">Editar</a>
-            <form action="{{ route('produtos.destroy', $p) }}" method="POST" style="display:inline">
-                @csrf @method('DELETE')
-                <button type="submit">Excluir</button>
-            </form>
-        </td>
-    </tr>
+        <tr>
+            <td>{{ $p->id }}</td>
+            <td>{{ $p->nome }}</td>
+            <td>{{ $p->quantidade }}</td>
+            <td>R$ {{ $p->preco }}</td>
+            <td>
+                <a href="{{ route('produtos.edit', $p) }}">Editar</a>
+                
+                <form action="{{ route('produtos.destroy', $p) }}" method="POST" style="display:inline">
+                    @csrf @method('DELETE')
+                    <button type="submit">Excluir</button>
+                </form>
+            </td>
+        </tr>
     @endforeach
+
 </table>
