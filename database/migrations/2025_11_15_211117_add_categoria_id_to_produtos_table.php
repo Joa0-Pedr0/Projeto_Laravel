ac5f41fa96d4f7ce047537267e07abb6f04c8b6f
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2025-11-15 20:42:38 -0300

    feito as migrations das categorias

---
+++ database/migrations/2025_11_15_211117_add_categoria_id_to_produtos_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+public function up(): void
+{
+    Schema::table('produtos', function (Blueprint $table) {
+        $table->foreignId('categoria_id')->nullable()->after('id')->constrained('categorias')->nullOnDelete();
+    });
+}
+
+public function down(): void
+{
+    Schema::table('produtos', function (Blueprint $table) {
+        $table->dropConstrainedForeignId('categoria_id');
+    });
+}
+};
